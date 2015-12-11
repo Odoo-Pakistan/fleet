f8eaf197ba5342fc2c4bd12ffa065a348b4cb73c
nemanja-d <nemanja-d@hotmail.com>
2015-12-11 18:01:57 +0100

    Prikaz polja diff_amount za driver_cash

--- fleet_travel_order_driver_cash/models/fleet_travel_order_driver_cash.py
+++ fleet_travel_order_driver_cash/models/fleet_travel_order_driver_cash.py
@@ -13,11 +13,18 @@ class FleetTravelOrderDriverCash(models.Model):
     def _compute_returned_amount(self):
         self.returned_amount = 0
 
+    @api.multi
+    @api.depends('received_amount', 'returned_amount')
+    def _compute_diff_amount(self):
+        for rec in self:
+            rec.diff_amount = (rec.received_amount or 0) - (rec.returned_amount or 0)
+
     name = fields.Char(compute=_get_name, string='Name')
     travel_order_id = fields.Many2one('fleet.vehicle.travel.order')
     currency_id = fields.Many2one('res.currency', string='Currency')
     received_amount = fields.Float('Received amount')
     returned_amount = fields.Float(string='Returned amount')
+    diff_amount = fields.Float(compute=_compute_diff_amount, string='Spent amount')
     currency_rate = fields.Float(string="Currency rate")
 #     cost_ids = fields.One2many('fleet.vehicle.cost', 'driver_cash_id', 'Costs paid')
 
