c99c7f40229310c832d4f119c3c6003f84b40b95
nemanja-d <nemanja-d@hotmail.com>
2015-12-14 22:11:45 +0100

    SQL reporti za izvjestaje

---
+++ fleet_compact_specific/sql/obracun_plate.sql
@@ -0,0 +1,16 @@
+﻿
+-- OVDJE TREBA DODATI JOS USLOV DA SE ODABERE MJESEC I VOZAC
+select 
+	row_number() OVER (ORDER BY tro.date asc) as "redni_broj",
+	tro.date as datum,
+	fak.broj_fakture,
+	tro.distance as relacija,
+	fak.prevezeno_tona,
+	fak."cijena_po_toni_EUR",
+	(fak.prevezeno_tona*fak."cijena_po_toni_EUR") as "vrijednost_EUR",
+	fak.ukupan_iznos as "vrijednost_KM",
+	(0.1*fak.ukupan_iznos) as zarada_vozaca
+	
+from fleet_invoice fak
+left join fleet_vehicle_travel_order tro on (tro.id = fak.travel_order_id)
+left join hr_employee emp on (emp.id = tro.driver1_id)
---
+++ fleet_compact_specific/sql/pregled_transporta.sql
@@ -0,0 +1,67 @@
+﻿select 	tbl.relacija, 
+	tbl.datum, 
+	tbl.prevezeno_tona, 
+	tbl.vrijednost_km, 
+	tbl.gorivo_litara, 
+	tbl.gorivo_vrijednost, 
+	(0.1453*tbl.gorivo_vrijednost) as gorivo_pdv,
+	(0.8547*tbl.gorivo_vrijednost) as gorivo_bez_pdv,
+	tbl.troskovi_akontacije,
+	tbl.troskovi_putarine,
+	(0.1*vrijednost_km) as zarada_vozaca,
+	tbl.troskovi_pranje_cisterne,
+	( 0.9*vrijednost_km - (tbl.troskovi_putarine+tbl.troskovi_akontacije+tbl.troskovi_pranje_cisterne+0.8547*tbl.gorivo_vrijednost) ) as ruc,
+	tbl.predjeno_km,
+	((tbl.gorivo_litara/tbl.predjeno_km)*100) as potrosnja_na_100_km
+	
+	
+from
+(
+	select
+	
+	tro.distance as relacija
+	,tro.date as datum
+	,(
+		select coalesce(sum(fak.prevezeno_tona), 0)
+		from fleet_invoice fak
+		where fak.travel_order_id = tro.id
+	) as prevezeno_tona
+	,(
+		select coalesce(sum(fak.ukupan_iznos), 0)
+		from fleet_invoice fak
+		where fak.travel_order_id = tro.id
+		
+	) as vrijednost_km
+	,(
+		select coalesce(sum(fuel.liter), 0)
+		from fleet_vehicle_log_fuel fuel
+		left join fleet_vehicle_cost cst on (cst.id = fuel.cost_id)
+		where fuel.travel_order_id = tro.id
+	) as gorivo_litara
+	,(
+		select coalesce(sum(cst.amount), 0)
+		from fleet_vehicle_cost cst
+		where cst.travel_order_id = tro.id and cst.cost_type = 'fuel'
+	) as gorivo_vrijednost
+	,(
+		select coalesce(sum(cst.amount), 0)
+		from fleet_vehicle_cost cst
+		left join fleet_service_type type on (type.id = cst.cost_subtype_id)
+		where cst.travel_order_id = tro.id and type.name like 'Putni troškovi'
+	) as troskovi_akontacije
+	,(
+		select coalesce(sum(cst.amount), 0)
+		from fleet_vehicle_cost cst
+		left join fleet_service_type type on (type.id = cst.cost_subtype_id)
+		where cst.travel_order_id = tro.id and type.name like 'Putarina'
+	) as troskovi_putarine
+	,(
+		select coalesce(sum(cst.amount), 0)
+		from fleet_vehicle_cost cst
+		left join fleet_service_type type on (type.id = cst.cost_subtype_id)
+		where cst.travel_order_id = tro.id and type.name like 'Pranje%'
+	) as troskovi_pranje_cisterne
+	, tro.total_km as predjeno_km
+
+	from fleet_vehicle_travel_order tro
+) as tbl
