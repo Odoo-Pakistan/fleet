89d32cc207ad27bd86b42e7ec7936cfff8e5e35d
nemanja-d <nemanja-d@hotmail.com>
2015-12-17 22:05:34 +0100

    Alerti dodati u res_config

--- fleet_extend/models/res_config.py
+++ fleet_extend/models/res_config.py
@@ -11,6 +11,7 @@ class FleetVehicleLogFuel(models.TransientModel):
     module_fleet_travel_order = fields.Boolean('Use travel orders', help='This installs module fleet_travel_order')
     module_fleet_travel_order_costs = fields.Boolean('Track costs via travel orders', help='This installs module fleet_travel_order_costs')
     module_fleet_travel_order_driver_cash = fields.Boolean('Track driver cash via travel order', help='This installs module fleet_travel_order_driver_cash')
+    module_fleet_alerts = fields.Boolean('Advanced features for vehicle alerts', help='This installs module fleet_alerts')
 
     @api.one
     @api.onchange('module_fleet_travel_order_driver_cash')
@@ -18,6 +19,11 @@ class FleetVehicleLogFuel(models.TransientModel):
         if self.module_fleet_travel_order_driver_cash:
             self.module_fleet_travel_order = True
 
+    @api.one
+    @api.onchange('module_fleet_alerts')
+    def change_fleet_alerts(self):
+        if self.module_fleet_alerts:
+            self.module_fleet_vehicle_cost_extend = True
 
     @api.one
     @api.onchange('module_fleet_travel_order_costs')
